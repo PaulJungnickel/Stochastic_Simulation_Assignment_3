77c12a83a64f9757dcd2d00a1c8160deb48616ba
PaulJungnickel <jungnikp@gmail.com>
2024-12-09 10:54:55 +0100

    move cooling / step schedule back out of class

--- CircleParticleSim.py
+++ CircleParticleSim.py
@@ -7,12 +7,38 @@ import scipy.spatial
 
 
 
+def basic_cooling_schedule(T, step):
+    return 0.999*T
+
+def paper_cooling_schedule(T, step):
+    if step % 100 == 0:
+        return 0.9*T
+    return T
+
+
+def log_cooling_schedule(T, step):
+    
+    return 1/(np.log(step + 3))
+
+
+def const_step_size_schedule(step, *args):
+    return 0.1
+
+def random_step_size_schedule(step, *args):
+    return np.random.rand()
+
+def hyperbolic_step_size_schedule(step, *args):
+    return np.maximum(0.001, 1./(1 + 0.1*step))
+
+def linear_step_size_schedule(step, *args):
+    return np.maximum(0.0001, 1 - 1/10000 * step)
+
 
 class CircleParticleSim:
     
     def __init__(self, N, initial_temperature = 10, steps=100, seed=42, 
-                 cooling_schedule = None,
-                 step_size_schedule = None
+                 cooling_schedule = basic_cooling_schedule,
+                 step_size_schedule = const_step_size_schedule
                  ) -> None:
         rand.seed(seed)
         #parameters for the simulation
@@ -20,20 +46,8 @@ class CircleParticleSim:
         self.T = initial_temperature
         self.initial_locations()
         self.initial_energy()
-
-
-        self.max_steps = steps
-
-        if cooling_schedule is None:
-            self.cooling_schedule = self.basic_cooling_schedule
-        else:
-            self.cooling_schedule = cooling_schedule
-            
-            
-        if cooling_schedule is None:
-            self.step_size_schedule = self.const_step_size_schedule
-        else:
-            self.step_size_schedule = step_size_schedule
+        self.cooling_schedule = cooling_schedule
+        self.step_size_schedule = step_size_schedule
 
         print('initial energy', self.E)
 
@@ -48,7 +62,7 @@ class CircleParticleSim:
         for i in range(steps):
             self.step = i
             self.single_move()
-            self.T = cooling_schedule(self)
+            self.T = self.cooling_schedule(self.T, i)
         
         self.plot_positions()
         print('mimimal energy', self.E)
@@ -107,7 +121,7 @@ class CircleParticleSim:
 
 
 
-        r =  self.step_size_schedule(self) * rand.rand()
+        r =  self.step_size_schedule(self.step) # * rand.rand()
         theta = 2*np.pi * rand.rand()     
 
         step = [r*np.cos(theta), r*np.sin(theta)]
@@ -133,32 +147,6 @@ class CircleParticleSim:
         
         self.energy_values[i] = self.E
         self.temp_values[i] = self.T
-
-
-
-
-
-    
-    def basic_cooling_schedule(self):
-        return 0.999*self.T
-
-    def paper_cooling_schedule(self):
-        if self.step % 100 == 0:
-            return 0.9*self.T
-        return self.T
-
-
-    def log_cooling_schedule(self):
-        
-        return 1/(np.log(self.step + 3))
-
-
-    def const_step_size_schedule(self):
-        return 0.1
-
-    def hyperbolic_step_size_schedule(self):
-        return 1./np.log(self.step+1)
-
 
 
         
